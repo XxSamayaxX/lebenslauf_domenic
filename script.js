b06b2671b726778e1949136a7426eb7e874a9021
XxSamayaxX <108954517+XxSamayaxX@users.noreply.github.com>
2022-10-31 15:51:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var widthSize = 99 + '%';
+document.getElementsByClassName('deutsch_wert')[0].style.width=widthSize;
+var widthSize = 80 + '%';
+document.getElementsByClassName('englisch_wert')[0].style.width=widthSize;  
